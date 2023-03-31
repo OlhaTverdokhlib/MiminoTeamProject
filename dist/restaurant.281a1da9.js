1798bce2278bf92c4f55e1dcfe0649f44675a7a5
Olha Tverdokhlib <balabolya@gmail.com>
2023-03-31 20:50:33 +0200

    last

---
+++ dist/restaurant.281a1da9.js
@@ -0,0 +1,21 @@
+AOS.init({
+    // Global settings:
+    disable: false,
+    startEvent: "DOMContentLoaded",
+    initClassName: "aos-init",
+    animatedClassName: "aos-animate",
+    useClassNames: false,
+    disableMutationObserver: false,
+    debounceDelay: 50,
+    throttleDelay: 99,
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120,
+    delay: 0,
+    duration: 1000,
+    easing: "ease",
+    once: false,
+    mirror: false,
+    anchorPlacement: "top-bottom"
+});
+
+//# sourceMappingURL=restaurant.281a1da9.js.map
